8f1ac14505b5d9cba0fc91b5ee876fbbbf8b3640
TesloKnife <teslo@example.com>
2025-08-24 21:48:52 +0500

    Добавление регистрации на backend, проверка уникальности логина, шифрование пароля. Маппинг данных пользователя, которые будут отображаться на frontend.

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const { register } = require("./controllers/user");
+const mapUser = require("./helpers/mapUser");
 
 const port = 3001;
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+
+// Подключение контроллера на post запрос
+app.post("/register", async (req, res) => {
+  try {
+    const user = await register(req.body.login, req.body.password);
+
+    res.send({ error: null, user: mapUser(user) });
+  } catch (error) {
+    res.send({ error: error.message || "Unknown error" });
+  }
+});
 
 mongoose
   .connect("mongodb://user:mongopass@localhost:27017/blog-app?authSource=admin")
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,19 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
 // register
+
+async function register(login, password) {
+  if (!password) {
+    throw new Error("Password is empty");
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ login, password: passwordHash });
+
+  return user;
+}
 
 // login
 
@@ -7,3 +22,7 @@
 // delete
 
 // edit (roles)
+
+module.exports = {
+  register,
+};
